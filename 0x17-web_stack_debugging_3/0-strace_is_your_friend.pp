9a256f56160653b1931be0c1d2b0fad9a4147c8e
nourhan <nourhansaeed89@gmail.com>
2024-04-15 06:59:31 -0700

    nn

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fixing Apache returning 500 error by correcting permissions on the PHP-FPM socket file
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
