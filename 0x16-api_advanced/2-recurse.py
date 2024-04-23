689827b682162bf421473ae84665a49cbe8afa96
nourhan <nourhansaeed89@gmail.com>
2024-04-23 11:15:14 -0700

    x

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,40 +1,38 @@
 #!/usr/bin/python3
+"""
+Queries the Reddit API and returns a list containing the titles of all hot
+articles for a given subreddit.
+"""
 import requests
-"""
-function that queries reddit api
-"""
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[], count=0, after=''):
     """
-    recursive function that queries the reddit api and returns a list
-    containing the titles of all hot articles for a given reddit.
-    if no results are found for the given subreddit the function
-    should return none
+    Queries the Reddit API and returns a list containing the titles of all hot
+    articles for a given subreddit.
     """
-    if type(subreddit) is list:
-        url = "https://api.reddit.com/r/{}?sort=hot".format(subreddit[0])
-        url = "{}&after={}".format(url, subreddit[1])
+    base = 'https://www.reddit.com/'
+    endpoint = 'r/{}/hot.json'.format(subreddit)
+    query_string = '?show="all"&limit=100&after={}&count={}'.format(
+        after, count)
+    url = base + endpoint + query_string
+    headers = {'User-Agent': 'Python/1.0(Holberton School 0x16 task 2)'}
+    response = requests.get(url, headers=headers)
+    if not response.ok:
+        if len(hot_list) == 0:
+            return None
+        else:
+            return hot_list
+
+    data = response.json()['data']
+    for post in data['children']:
+        hot_list.append(post['data']['title'])
+    after = data['after']
+    dist = data['dist']
+    if (after):
+        recurse(subreddit, hot_list, count + dist, after)
+
+    if len(hot_list) == 0:
+        return None
     else:
-        url = "https://api.reddit.com/r/{}?sort=hot".format(subreddit)
-        subreddit = [subreddit, ""]
-    header = {'User-Agent': 'CustomClient/1.0'}
-    req = requests.get(url, headers=header, allow_redirects=False)
-    if req.status_code != 200:
-        return (None)
-    req = req.json()
-    if "data" in req:
-        data = req.get("data")
-        if not data.get("children"):
-            return (hot_list)
-        for post in data.get("children"):
-            hot_list += [post.get("data").get("title")]
-        if not data.get("after"):
-            return (hot_list)
-        subreddit[1] = data.get("after")
-        recurse(subreddit, hot_list)
-        if hot_list[-1] is None:
-            del hot_list[-1]
-        return (hot_list)
-    else:
-        return (None)
+        return hot_list
