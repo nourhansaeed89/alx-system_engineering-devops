6b6463ffefae599f063bc3dd00edea879883b6ab
nourhan <nourhansaeed89@gmail.com>
2024-04-23 11:53:41 -0700

    c

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,6 +15,6 @@ def number_of_subscribers(subreddit):
     headers = {'User-Agent': 'Python/1.0(Holberton School 0x16 task 0)'}
     response = requests.get(url, headers=headers)
     if (not response.ok):
-        return 0
+        return (0)
     subscriber_count = response.json().get('data').get('subscribers')
-    return subscriber_count
+    return (subscriber_count)
